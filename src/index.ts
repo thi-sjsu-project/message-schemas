f36172615f957f5e20440813ed69f5dd41ea2ffe
thetek <git@thetek.de>
2024-04-01 16:43:51 +0200

    feat: initial schemas and sample convos

---
+++ src/index.ts
@@ -0,0 +1,180 @@
+import { AcaDefect, AcaFuelLow, AcaHeadingToBase, Message, MissileToOwnshipDetected, RequestApprovalToAttack } from "./schema";
+
+const samples: Array<Message> = [
+  // example convo 1: high priority, low threat, no collateral
+  {
+    id: 0,
+    priority: 8,
+    kind: "RequestApprovalToAttack",
+    data: {
+      target: {
+        location: { lat: 48.600045, lng: 11.607559 },
+        threatLevel: 0.2,
+        type: "RS-12",
+      },
+      collateralDamage: "none",
+      detectedByAca: 4,
+      attackWeapon: {
+        type: "ewSuppression",
+        load: 0.6,
+      },
+      choiceWeight: 0.5,
+    },
+  } satisfies RequestApprovalToAttack,
+
+  // example convo 2: low priority, low threat, no collateral, weapons low
+  {
+    id: 0,
+    priority: 2,
+    kind: "RequestApprovalToAttack",
+    data: {
+      target: {
+        location: { lat: 48.648044, lng: 11.594554 },
+        threatLevel: 0.25,
+        type: "RS-10",
+      },
+      collateralDamage: "none",
+      detectedByAca: 2,
+      attackWeapon: {
+        type: "kinetic",
+        load: 0.1,
+      },
+      choiceWeight: -0.2,
+    },
+  } satisfies RequestApprovalToAttack,
+
+  // fuel low on aca
+  {
+    id: 0,
+    priority: 5,
+    kind: "AcaFuelLow",
+    data: {
+      acaId: 5,
+      fuelLevel: 0.1,
+    }
+  } satisfies AcaFuelLow,
+
+  // example convo 3: low priority, low threat, no collateral
+  {
+    id: 0,
+    priority: 3,
+    kind: "RequestApprovalToAttack",
+    data: {
+      target: {
+        location: { lat: 48.630549, lng: 11.583752 },
+        threatLevel: 0.15,
+        type: "AR-5",
+      },
+      collateralDamage: "none",
+      detectedByAca: 1,
+      attackWeapon: {
+        type: "ewSuppression",
+        load: 0.8,
+      },
+      choiceWeight: 0.3,
+    },
+  } satisfies RequestApprovalToAttack,
+
+  // aca that previously send low fuel is now heading to base
+  {
+    id: 0,
+    priority: 5,
+    kind: "AcaHeadingToBase",
+    data: {
+      acaId: 5,
+      reason: "fuelLow",
+    }
+  } satisfies AcaHeadingToBase,
+
+  // example convo 4: low priority, high threat, no collateral
+  {
+    id: 0,
+    priority: 3,
+    kind: "RequestApprovalToAttack",
+    data: {
+      target: {
+        location: { lat: 48.607569, lng: 11.582834 },
+        threatLevel: 0.9,
+        type: "RS-12",
+      },
+      collateralDamage: "none",
+      detectedByAca: 2,
+      attackWeapon: {
+        type: "ewSuppression",
+        load: 0.7,
+      },
+      choiceWeight: 0,
+    },
+  } satisfies RequestApprovalToAttack,
+
+  // example convo 5: missile heading towards ownship, weapons low on aca
+  {
+    id: 0,
+    priority: 10,
+    kind: "MissileToOwnshipDetected",
+    data: {
+      missileLocation: { lat: 48.603579, lng: 11.592345 },
+      survivability: 0.8,
+      detectedByAca: 1,
+      acaAttackWeapon: {
+        type: "veryIntimidatingWeaponName",
+        load: 0.2,
+      },
+      choiceWeight: 1,
+    },
+  } satisfies MissileToOwnshipDetected,
+
+  // defect on aca
+  {
+    id: 0,
+    priority: 7,
+    kind: "AcaDefect",
+    data: {
+      acaId: 1,
+      message: "A screw came loose mid-flight.",
+    },
+  } satisfies AcaDefect,
+
+  // example convo 6: high priority, low threat, possible collateral
+  {
+    id: 0,
+    priority: 8,
+    kind: "RequestApprovalToAttack",
+    data: {
+      target: {
+        location: { lat: 48.599382, lng: 11.593753 },
+        threatLevel: 0.1,
+        type: "DRDX-1",
+      },
+      collateralDamage: "complex",
+      detectedByAca: 3,
+      attackWeapon: {
+        type: "kinetic",
+        load: 0.5,
+      },
+      choiceWeight: -0.6,
+    },
+  } satisfies RequestApprovalToAttack,
+
+  // example convo 7: high priority, low threat, no collateral, detected by ownship
+  {
+    id: 0,
+    priority: 7,
+    kind: "RequestApprovalToAttack",
+    data: {
+      target: {
+        location: { lat: 48.599382, lng: 11.593753 },
+        threatLevel: 0.15,
+        type: "DRDX-1",
+      },
+      collateralDamage: "none",
+      attackWeapon: {
+        type: "kinetic",
+        load: 0.5,
+      },
+      choiceWeight: 0.4,
+    },
+  } satisfies RequestApprovalToAttack,
+];
+
+console.log(samples);
